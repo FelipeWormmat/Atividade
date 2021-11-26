3138b18475fcb4baff61a76a9af3c56d9236d47a
Felipe <felipe.wormmat@gmail.com>
2021-11-26 16:33:06 -0300

    Acelera

---
+++ main.py
@@ -0,0 +1,103 @@
+from carrinho import Carrinho, Cliente, Produto
+
+class Valores:
+    def mensagem_inteira(descricao):
+        while True:
+            try:
+                return int(input(descricao))
+            except:
+                print('Informe um valor numérico')
+    def mensagem(descricao):
+        while True:
+            try:
+                return (input(descricao))
+            except:
+                print('Informe um valor numérico')
+class Aplicacao:
+    comandos = [
+        (0, 'Informar cliente'),
+        (1, 'Adicionar produto'),
+        (2, 'Incrementar item'),
+        (3, 'Decrementar item'),
+        (4, 'Remover item'),
+        (5, 'Exibir resumo')
+    ]
+
+    def __init__(self):
+        self._carrinho = Carrinho()
+
+    def __init__(self):
+        self._cliente = Cliente()
+
+    def __init__(self):
+        self._produto = Produto()
+
+    def executar(self):
+        while True:
+            self._exibir_menu()
+            comando = input('> Selecione uma opcao acima: ')
+            deve_sair = self.executar_comando(comando)
+            if deve_sair:
+                break
+
+    def executar_comando(self, comando):
+        if comando == '0':
+            self._inserir_cliente()
+        elif comando == '1':
+            self._adicionar_produto()
+        elif comando == '2':
+            self._incrementar_item()
+        elif comando == '3':
+            self._decrementar_item()
+        elif comando == '4':
+            self._remover_item()
+        elif comando == '5':
+            self._exibir_resumo()
+        else:
+            return True
+
+    def _inserir_cliente(self):
+        nome = input('> Informe seu nome: ')
+        identidade = input('> Informe seu identidade: ')
+        cliente = Cliente(nome, identidade)
+        self._carrinho.cliente = cliente
+
+    def _adicionar_produto(self):
+        print('Informe os dados: ')
+        descricao = input('Descrição: ')
+        codigo = input('Código: ')
+        try: 
+            quantidade = Valores.mensagem_inteira("Quantidade: ")
+            desconto = Valores.mensagem_inteira("Desconto: ")
+            valor_unitario = Valores.mensagem("Valor Unitário: ")
+        except ValueError:
+            print("Informe um valor númerico!!!!")
+
+        produto = Produto(codigo=codigo, descricao=descricao, desconto=desconto,
+                          valor_unitario=valor_unitario, quantidade=quantidade)
+        self._carrinho.add_item(produto)
+
+    def _incrementar_item(self):
+        codigo = input('Codigo: ')
+        self._carrinho.incrementar_item(codigo)
+
+    def _decrementar_item(self):
+        codigo = input('Código: ')
+        self._carrinho._decrementar_item(codigo)
+
+    def _remover_items(self):
+        codigo = input('Código: ')
+        self._carrinho.remover_item(codigo)
+
+    def _exibir_resumo(self):
+        self._carrinho.exibir_resumo()
+
+    def _exibir_menu(self):
+        for op in Aplicacao.comandos:
+            opcao, descricao = op
+            print(f'[{opcao}] - {descricao}')
+
+
+app = Aplicacao()
+
+app.executar()
